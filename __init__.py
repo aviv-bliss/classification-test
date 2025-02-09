3fe2e9bbb141e744d75db616dc05ae0f210101b1
Aviv Ben Yosef <avivbyo@gmail.com>
2025-02-09 16:11:10 +0200

    more

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
 from .node import *
 from .install import *
 
+comfy_path = os.path.dirname(folder_paths.__file__)
+modules_path = os.path.join(os.path.dirname(__file__), "modules")
 
+sys.path.append(modules_path)
 NODE_CLASS_MAPPINGS = {
     'ClassificationNode': ClassificationNode
 }
@@ -11,3 +14,14 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+
+try:
+    import cm_global
+    cm_global.register_extension('Bliss Classification',
+                                 {'version': "1",
+                                  'name': 'Impact Pack',
+                                  'nodes': set(NODE_CLASS_MAPPINGS.keys()),
+                                  'description': 'Bliss Comfy Nodes', })
+except:
+    pass
